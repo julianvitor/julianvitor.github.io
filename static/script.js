fdbee68e794c1e6ec3efb6388e56751eac8e7d27
Julian Carreiro <julian.vtr@gmail.com>
2023-12-27 02:30:10 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -79,7 +79,7 @@ class GardenApp {
 
     fetchAndUpdateSensorData() {
         // Busca e Atualização de Dados do Sensor
-        fetch('http://julianvitor.sytes.net:8082/sensor-data')
+        fetch('http://julianvitor.sytes.net:8000/sensor-data')
             .then((response) => response.json())
             .then((data) => {
                 this.updateSensorData(data);
@@ -236,7 +236,7 @@ class GardenApp {
     }
     waterPlant() {
         // Irrigação da Planta
-        fetch("http://julianvitor.sytes.net:8082/water-plant", {
+        fetch("http://julianvitor.sytes.net:8000/water-plant", {
             method: "POST",
         })
         .then((response) => response.json())
